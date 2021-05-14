42e514e546beda4e056beb3eb8bf4812e763139b
Ben Rasmussen <rasmussen.ben32@gmail.com>
2021-05-14 11:51:04 -0500

    removed details.js, working on framing character.js

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,21 @@
 // Write your Character component here
+import React, { useState, useEffect } from 'react'
+import { BASE_URL } from './constants'
+import axios from 'axios'
+
+export default function Details(props) {
+    const { characterId, close } = props
+    const [details, setDetails] = useState(null)
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}`)
+            .then(res => { setDetails(res.data)})
+            .catch(err => console.log('Error: failed to retrieve', err))
+    }, [characterId])
+
+    return (
+        <div className='container'>
+            <h2></h2>
+        </div>
+    )
+}
