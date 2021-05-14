d0cdfd6aec4dd83d27be1f1eac4a7cd3388336b9
Ben Rasmussen <rasmussen.ben32@gmail.com>
2021-05-14 12:17:52 -0500

    working on getEffect loop, troubleshooting character.js compiler

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import axios from 'axios'
-import { BASE_URL } from './constants'
+import axios from 'axios';
+import { BASE_URL } from './constants';
+import Character from './components/Character';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -10,6 +11,14 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+
+  const [character, setCharacter] = useState(null);
+
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people')
+      .then(res => { setCharacter(res.data) })
+      .catch(err => console.log('Error: failed to retrieve', err))
+  }, []);
 
   return (
     <div className="App">
